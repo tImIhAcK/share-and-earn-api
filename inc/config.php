f188ee7d435f2ec9f22f3ac8ba186b6d434ade2f
Adeniran John <adeniranjohn2016@gmail.com>
2022-09-19 23:53:27 +0100

    removed inc

--- inc/config.php
+++
@@ -1,9 +0,0 @@
-<?php 
-
-define('DB_HOST', 'localhost');
-define('DB_USER', 'root');
-define('DB_PASS', '');
-define('DB_NAME', 'ubuy_chair');
-define('MESSAGE_DATABASE_ERROR', "Couldn't connect to database")
-
-?>
